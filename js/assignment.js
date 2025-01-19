af1e502d9bd220f630076e4849861be39c570a9a
Alicia Bures <arosebures@gmail.com>
2025-01-19 18:36:53 -0500

    Completed part two and updated readme

--- js/assignment.js
+++ js/assignment.js
@@ -1,22 +1,33 @@
 "use strict";
+/*  - [ ] Create an immutable variable that will store a reference to the paragraph tag with the id of `response`.
+    - [ ] Create a mutable variable called `age`, but do not assign it a value.
+    - [ ] Write your conditional logic inside the `checkAgeAndRespond` function.
+    - [ ] Read and try to understand the code that exists already. Write notes/questions and bring them to class.*/
 
 // Create your references to the html elements here
-const ageInputEl = document.getElementById("age-input")
+const ageInputEl = document.getElementById("age-input");
 const submissionBtn = document.getElementById("submission-btn");
+const response = document.getElementById("response");
 
 // create a mutable variable called age and do not assign it a value.
 
+let age;
 
 function checkAgeAndRespond() {
     age = parseInt(ageInputEl.value);
-    
-    // Write your code below
-
+}
+if (age >= 21) {
+    console.log("You can vote and purchase alcohol.")
+} 
+    else if (age >= 18 && age < 21) {
+    console.log ("You can vote, but you cannot purchase alcohol.")
+} else { console.log("You cannot vote and you cannot purchase alcohol.");
 }
 
 
 submissionBtn.addEventListener("click", function () {
     checkAgeAndRespond();
+
 });
 
 
@@ -28,3 +39,8 @@ function validateInput(event) {
 }
 
 ageInputEl.addEventListener("keydown", validateInput);
+
+    
+
+
+
