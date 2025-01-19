867322d5d33ecc1e4cd5aabc9a3cfbe37ada27ae
Alicia Bures <arosebures@gmail.com>
2025-01-19 16:24:57 -0500

    Completed part one and updated readme

--- js/index.js
+++ js/index.js
@@ -1,3 +1,39 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+/* "use strict"
+
+let healthPoints = 25;
+
+if (healthPoints >= 10) {
+    console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+    console.log("Hero is slightly damaged.");
+} else {
+    console.log("Hero is close to death.");
+
+}
+
+let review = 49;
+
+if (review >= 90){
+    console.log("Excellent");
+} else if (review < 90 && review >= 50) {
+    console.log("This is okay");
+} else {
+    console.log("Needs improvement");
+}
+
+let myname = "Ben";
+if (myname == "Alicia") {
+    console.log ("Hello Alicia");
+} else{
+    console.log("Who is this?");
+}
+
+let color = "red"
+if (color == "green") {
+    console.log ("The leaves are green");
+} else if (color == "brown") {
+    console.log("The leaves are brown");
+} else { console.log("I can't see the leaves")}
+ */
